d94ff5cc9251a384ce3dbbd16ac0213212e89368
Lori Burleson <lori.burleson@gmail.com>
2016-10-20 17:15:02 -0400

    Ingredient and Drink seeds ready

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,7 @@
 class Ingredient < ApplicationRecord
-  validates :name, :category_id, presence: true
+  validates :name, presence: true
 
   belongs_to :mixed_drink
-  has_one :category
   has_man :drinks, through: :mixed_drinks
 
 end
