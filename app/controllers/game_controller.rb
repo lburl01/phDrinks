6339fe2dfdb5f243765dec7eee110098a23f1728
Lori Burleson <lori.burleson@gmail.com>
2016-10-21 16:56:19 -0400

    Added game new view and added button on user/home to go to game/new

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,8 +1,5 @@
 class GameController < ApplicationController
 
-  def index
-    # @games = Game.all_games(current_user)
-    # @unique_games = @games.map{ |game| game.session }
-    # redirect_to '/'
+  def new
   end
 end
