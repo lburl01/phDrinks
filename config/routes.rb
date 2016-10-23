7b2998776f86ffb2d98251829d90746b946b3448
Lori Burleson <lori.burleson@gmail.com>
2016-10-23 12:43:31 -0400

    Fixed routing problem on home#index; had changed to home#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users # once devise is done signing me up or in, it will go to root (or whatever's next)
+  devise_for :users
   root to: "home#index"
 
   get 'game/index'
