bf5598019b60f6a53efaf8e808f80210751fb2bb
SamPlifier <sam.davis.castro@gmail.com>
2016-10-24 09:06:04 -0400

    added logo and fade in to login page

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -1,1 +1,13 @@
 console.log('Devise!');
+
+// include code below on every html page you add js things to
+// <% content_for :javascripts do %>
+// <%= javascript_include_tag 'devise', 'data-turbolinks-track': 'reload' %>
+// <% end %>
+
+// add this to the bottom of the html page
+// <%= javascript_include_tag "jquery-3.1.1.min.js" %>
+
+/*! Fades in page on load */
+$('body').css('display', 'none');
+$('body').fadeIn(2000);
